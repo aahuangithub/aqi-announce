0b024ff335824de2bda0e92d365439077c48deaf
aaron <aahuang@berkeley.edu>
2018-11-15 00:11:11 -0800

    fix over 200

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ from gtts import gTTS
 url = 'https://www.wunderground.com/health/us/ca/berkeley/94701'
 request = requests.get(url)
 msg = BeautifulSoup(request.text)
+aqi = int(msg.find_all('div', class_="aqi-value")[0].text)
 
-while not msg.find(text='200'):
+while aqi < 200:
     time.sleep(30)
     request = requests.get(url)
     if request.status_code != 200:
         print('error getting aqi')
         break
     msg = BeautifulSoup(request.text)
+    aqi = int(msg.find_all('div', class_="aqi-value")[0].text)
     os.system('cls')
     print('aqi still not over 200 :(')
+
+os.system('cls')
+print('aqi is '+str(aqi))
 
 tts = gTTS(text="AQI is over 200! berkeley is cancelled", lang='en')
 tts.save("pcvoice.mp3")
